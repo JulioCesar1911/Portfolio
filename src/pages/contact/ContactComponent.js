5592da4597d1ec290f5a3df60663a4d9979afca8
Julio Morales <jcmoralesvargas@gmail.com>
2024-05-22 07:01:06 -0400

    final email

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -121,7 +121,7 @@ class Contact extends Component {
                 </p>
                 <div className="address-btn-div">
                   <Button
-                    text="Visit on Google Maps"
+                    text="Send me an Email"
                     newTab={true}
                     href={addressSection.location_map_link}
                     theme={theme}
